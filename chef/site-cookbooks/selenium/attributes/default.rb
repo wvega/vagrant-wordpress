f1789002b3eb4765cf14b1cb7fe81ebbf81d2b64
Willington Vega <wvega@wvega.com>
2014-02-07 23:11:36 -0500

    Drop chef modules. We are now going to use an script.

--- chef/site-cookbooks/selenium/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default['selenium']['server']['install_path'] = "/usr/local/selenium-server"
-default['selenium']['server']['log_path'] = "/var/log/selenium"
-default['selenium']['server']['version'] = "2.33.0"
